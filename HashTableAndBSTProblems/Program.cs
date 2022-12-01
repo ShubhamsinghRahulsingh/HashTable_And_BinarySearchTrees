c43aceae92e332584c8482d4cdd9b17bb928e710
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-01 15:08:20 +0530

    (Shubham)Add: Project folder structure created

---
+++ HashTableAndBSTProblems/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HashTableAndBSTProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hash table And Binary Search Tree(BST) Problems");
+        }
+    }
+}
